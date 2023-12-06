dcf3111af6cf435995e328ec478ce1ea5e7ed7b7
mahmoud <awad25.ma@gmail.com>
2023-12-06 14:16:52 -0500

    wip added components

---
+++ frontend/components/global/LoadingState.vue
@@ -0,0 +1,20 @@
+<script setup>
+const { loading } = storeToRefs(useAuthStore());
+</script>
+<template>
+  <div v-if="loading" class="bg-gray h-2">
+    <div
+      class="duration-400 animate-loader bg-brandAccent h-2 transition-all"
+    ></div>
+  </div>
+</template>
+<style>
+@keyframes spin {
+  0% {
+    width: 0%;
+  }
+  100% {
+    width: 100%;
+  }
+}
+</style>
---
+++ frontend/components/global/MdModal.vue
@@ -0,0 +1,109 @@
+<template>
+  <teleport to="body">
+    <transition leave-active-class="duration-200">
+      <div
+        v-show="show"
+        class="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto px-4 py-6 sm:px-0"
+        scroll-region
+      >
+        <transition
+          enter-active-class="duration-300 ease-out"
+          enter-from-class="opacity-0"
+          enter-to-class="opacity-100"
+          leave-active-class="duration-200 ease-in"
+          leave-from-class="opacity-100"
+          leave-to-class="opacity-0"
+        >
+          <div
+            v-show="show"
+            class="fixed inset-0 transform transition-all"
+            @click="close"
+          >
+            <div class="absolute inset-0 bg-gray-500 opacity-75" />
+            <div
+              class="absolute inset-10 flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-primary font-bold text-white hover:opacity-70"
+            >
+              x
+            </div>
+          </div>
+        </transition>
+
+        <transition
+          enter-active-class="duration-300 ease-out"
+          enter-from-class="translate-y-4 opacity-0 sm:translate-y-0 sm:scale-95"
+          enter-to-class="translate-y-0 opacity-100 sm:scale-100"
+          leave-active-class="duration-200 ease-in"
+          leave-from-class="translate-y-0 opacity-100 sm:scale-100"
+          leave-to-class="translate-y-4 opacity-0 sm:translate-y-0 sm:scale-95"
+        >
+          <div
+            v-show="show"
+            class="w-full transform overflow-hidden rounded-lg bg-white shadow-xl transition-all sm:mx-auto sm:w-full"
+            :class="maxWidthClass"
+          >
+            <slot v-if="show" />
+          </div>
+        </transition>
+      </div>
+    </transition>
+  </teleport>
+</template>
+
+<script setup>
+const props = defineProps({
+  show: {
+    type: Boolean,
+    default: false,
+  },
+  maxWidth: {
+    type: String,
+    default: "2xl",
+  },
+  closeable: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const emit = defineEmits(["close"]);
+
+watch(
+  () => props.show,
+  () => {
+    if (props.show) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = null;
+    }
+  }
+);
+
+const close = () => {
+  if (props.closeable) {
+    emit("close");
+  }
+};
+
+const closeOnEscape = (e) => {
+  if (e.key === "Escape" && props.show) {
+    close();
+  }
+};
+
+onMounted(() => document.addEventListener("keydown", closeOnEscape));
+
+onUnmounted(() => {
+  document.removeEventListener("keydown", closeOnEscape);
+  document.body.style.overflow = null;
+});
+
+const maxWidthClass = computed(() => {
+  return {
+    sm: "sm:max-w-sm",
+    md: "sm:max-w-md",
+    lg: "sm:max-w-lg",
+    xl: "sm:max-w-xl",
+    "2xl": "sm:max-w-2xl",
+  }[props.maxWidth];
+});
+</script>
