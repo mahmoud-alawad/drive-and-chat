5999abf509dd3379bc62d816964b3e2cb439eb6a
mahmoud <awad25.ma@gmail.com>
2023-12-10 17:18:00 -0500

    initial useSocketio

---
+++ frontend/composables/useSocket.ts
@@ -0,0 +1,15 @@
+import { io } from "socket.io-client";
+
+export const useSocket = () => {
+  const config = useRuntimeConfig();
+  const userConnected = ref(false);
+  const ioSocket = io(config.public.apiUrl, {
+    extraHeaders: {
+      Authorization: `Bearer ${useCookie("token").value}`,
+    },
+  });
+  ioSocket.on("connect", (data) => {
+    userConnected.value = data;
+  });
+  return { ioSocket };
+};
