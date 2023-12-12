3370ae0e089342ebf74bb066c854e738f0ddedd4
mahmoud <awad25.ma@gmail.com>
2023-12-12 17:43:02 -0500

    notification

--- frontend/composables/useSocket.ts
+++ frontend/composables/useSocket.ts
@@ -2,14 +2,35 @@ import { io } from "socket.io-client";
 
 export const useSocket = () => {
   const config = useRuntimeConfig();
+  const router = useRouter();
+  const isNotInChat = router.currentRoute.value.matched.some(
+    (i) => i.path !== useLocalePath()("/dashboard/chat/:id()")
+  );
+
   // const userConnected = ref(false);
   const ioSocket = io(config.public.apiUrl, {
     extraHeaders: {
       Authorization: `Bearer ${useCookie("token").value}`,
     },
   });
+  ioSocket.on("receiveMessage", (_data) => {
+    if (isNotInChat) {
+      const audio = new Audio(
+        "https://assets.mixkit.co/active_storage/sfx/2841/2841-preview.mp3"
+      );
+      audio.play();
+      // eslint-disable-next-line no-new
+      const notification = new Notification("New message", {
+        body: "You have a new message!",
+        icon: "https://icons8.com/icon/124405/message-link",
+      });
+      notification.addEventListener("click", () => {
+        navigateTo(useLocalePath()("/dashboard/chat"));
+      });
+    }
+  });
   // ioSocket.on("connect", (data) => {
   //   userConnected.value = data;
   // });
-  return { ioSocket };
+  return { ioSocket, isNotInChat };
 };
