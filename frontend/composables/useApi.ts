f8855d87012019b84f40e719230082f62e6a9173
mahmoud <malawad@dmind.it>
2023-12-15 20:53:17 +0100

    fixes on useApi

--- frontend/composables/useApi.ts
+++ frontend/composables/useApi.ts
@@ -1,20 +1,24 @@
-import { useToken } from "./useToken";
-
 export const useApi = (
   request: string,
   fetchOptions: { [key: string]: any } = {}
 ) => {
-  const apiUrl = "http://0.0.0.0:3001/api/";
-  const { token } = useToken();
+  const config = useRuntimeConfig();
+  const apiUrl = config.public.apiUrl;
+  const token = useCookie("token");
 
   fetchOptions.headers = {
     ...fetchOptions.headers,
   };
+
+  fetchOptions.immediate = true;
+
   if (token && token.value) {
     fetchOptions.headers = {
       ...fetchOptions?.headers,
+      "Access-Control-Allow-Origin": "*",
       Authorization: `Bearer ${token.value}`,
     };
   }
-  return $fetch(apiUrl + request, { ...fetchOptions });
+
+  return useFetch(apiUrl + request, { ...fetchOptions });
 };
