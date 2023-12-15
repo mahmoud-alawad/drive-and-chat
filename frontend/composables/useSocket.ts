06b4d32f6921af0d205a9dcc8a09c40b2fb1defe
mahmoud <malawad@dmind.it>
2023-12-15 20:54:29 +0100

    minor fixes

--- frontend/composables/useSocket.ts
+++ frontend/composables/useSocket.ts
@@ -3,23 +3,27 @@ import { io } from "socket.io-client";
 export const useSocket = () => {
   const config = useRuntimeConfig();
   const router = useRouter();
+  const permessions = ref<string>();
+  const isConntected = ref<boolean>();
   const isNotInChat = router.currentRoute.value.matched.some(
     (i) => i.path !== useLocalePath()("/dashboard/chat/:id()")
   );
 
-  // const userConnected = ref(false);
   const ioSocket = io(config.public.apiUrl, {
+    // reconnection: false,
     extraHeaders: {
       Authorization: `Bearer ${useCookie("token").value}`,
     },
   });
+  console.log(ioSocket);
+  isConntected.value = ioSocket.connected;
   ioSocket.on("receiveMessage", (_data) => {
-    if (isNotInChat) {
+    if (isNotInChat && permessions.value === "granted") {
       const audio = new Audio(
         "https://assets.mixkit.co/active_storage/sfx/2841/2841-preview.mp3"
       );
       audio.play();
-      // eslint-disable-next-line no-new
+
       const notification = new Notification("New message", {
         body: "You have a new message!",
         icon: "https://icons8.com/icon/124405/message-link",
@@ -29,8 +33,11 @@ export const useSocket = () => {
       });
     }
   });
+  // ioSocket.on("connect_error", (err) => {
+  //   console.log(`connect_error due to ${err}`);
+  // });
   // ioSocket.on("connect", (data) => {
   //   userConnected.value = data;
   // });
-  return { ioSocket, isNotInChat };
+  return { ioSocket, isNotInChat, isConntected, permessions };
 };
