97b457543cb4d70da9c3dcb43e109637defd70f5
mahmoud <awad25.ma@gmail.com>
2023-12-16 10:30:39 -0500

    added back history component

---
+++ frontend/components/global/BackArrow.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const router = useRouter();
+const back = () => {
+  router.back();
+};
+</script>
+
+<template>
+  <div
+    class="mt-3 w-fit rounded-md bg-gray-200 px-6 py-1 hover:bg-gray-100"
+    @click="back"
+  >
+    <Icon class="text-lg font-bold" name="carbon:arrow-left" />
+  </div>
+</template>
+
+<style scoped></style>
